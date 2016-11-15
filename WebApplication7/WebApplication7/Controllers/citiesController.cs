2a05b306427b7d802232f3cbfba2dc79ac438283
VishavPremlall <VishavP@sahomeloans.com>
2016-11-15 13:38:17 +0200

    css & mysql demo
    css & mysql

---
+++ WebApplication7/WebApplication7/Controllers/citiesController.cs
@@ -0,0 +1,128 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class citiesController : Controller
+    {
+        private worldEntities db = new worldEntities();
+
+        // GET: cities
+        public ActionResult Index()
+        {
+            var city = db.city.Include(c => c.country);
+            return View(city.ToList());
+        }
+
+        // GET: cities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            city city = db.city.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // GET: cities/Create
+        public ActionResult Create()
+        {
+            ViewBag.CountryCode = new SelectList(db.country, "Code", "Name");
+            return View();
+        }
+
+        // POST: cities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,CountryCode,District,Population")] city city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.city.Add(city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryCode = new SelectList(db.country, "Code", "Name", city.CountryCode);
+            return View(city);
+        }
+
+        // GET: cities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            city city = db.city.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CountryCode = new SelectList(db.country, "Code", "Name", city.CountryCode);
+            return View(city);
+        }
+
+        // POST: cities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,CountryCode,District,Population")] city city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(city).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CountryCode = new SelectList(db.country, "Code", "Name", city.CountryCode);
+            return View(city);
+        }
+
+        // GET: cities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            city city = db.city.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // POST: cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            city city = db.city.Find(id);
+            db.city.Remove(city);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication7/WebApplication7/Controllers/countrylanguagesController.cs
@@ -0,0 +1,128 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class countrylanguagesController : Controller
+    {
+        private worldEntities db = new worldEntities();
+
+        // GET: countrylanguages
+        public ActionResult Index()
+        {
+            var countrylanguage = db.countrylanguage.Include(c => c.country);
+            return View(countrylanguage.ToList());
+        }
+
+        // GET: countrylanguages/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            countrylanguage countrylanguage = db.countrylanguage.Find(id);
+            if (countrylanguage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(countrylanguage);
+        }
+
+        // GET: countrylanguages/Create
+        public ActionResult Create()
+        {
+            ViewBag.CountryCode = new SelectList(db.country, "Code", "Name");
+            return View();
+        }
+
+        // POST: countrylanguages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CountryCode,Language,IsOfficial,Percentage")] countrylanguage countrylanguage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.countrylanguage.Add(countrylanguage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryCode = new SelectList(db.country, "Code", "Name", countrylanguage.CountryCode);
+            return View(countrylanguage);
+        }
+
+        // GET: countrylanguages/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            countrylanguage countrylanguage = db.countrylanguage.Find(id);
+            if (countrylanguage == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CountryCode = new SelectList(db.country, "Code", "Name", countrylanguage.CountryCode);
+            return View(countrylanguage);
+        }
+
+        // POST: countrylanguages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CountryCode,Language,IsOfficial,Percentage")] countrylanguage countrylanguage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(countrylanguage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CountryCode = new SelectList(db.country, "Code", "Name", countrylanguage.CountryCode);
+            return View(countrylanguage);
+        }
+
+        // GET: countrylanguages/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            countrylanguage countrylanguage = db.countrylanguage.Find(id);
+            if (countrylanguage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(countrylanguage);
+        }
+
+        // POST: countrylanguages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            countrylanguage countrylanguage = db.countrylanguage.Find(id);
+            db.countrylanguage.Remove(countrylanguage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
